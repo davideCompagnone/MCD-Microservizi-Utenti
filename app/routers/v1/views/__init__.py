8f814900fea9337a8daf834b10ed10e7136d0d65
FedericoCantarelli <79110279+FedericoCantarelli@users.noreply.github.com>
2024-06-23 22:14:14 +0200

    Aggiunto check per salute api

--- app/routers/v1/views/__init__.py
+++ app/routers/v1/views/__init__.py
@@ -1,4 +1,5 @@
 from .error import ErrorResponse
 from .ready import ReadyResponse
+from .health import HealthResponse
 
-__all__ = ("ErrorResponse", "ReadyResponse")
+__all__ = ("ErrorResponse", "ReadyResponse", "HealthResponse")
